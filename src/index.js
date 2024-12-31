569046db2ed5b1e0fb5fe35a1d0c26db6650b7c0
Lerian Febriana <kanglerian@gmail.com>
2024-12-31 11:01:04 +0700

    hide menu

--- src/index.js
+++ src/index.js
@@ -15,13 +15,15 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.removeMenu();
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
